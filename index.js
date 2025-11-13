57ca7604a2f6700c5b8b31ded60938ea63e8dbbc
TOM-BOSS-pinetour <b94889340@gmail.com>
2025-11-13 12:24:24 +0800

    first commit backend

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 1000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("hello world, hi 12 running");
+  console.log("req", req);
+});
+
+app.listen(PORT, () => {
+  console.log(`API listening on http://localhost:${PORT} `);
+});
