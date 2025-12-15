d3bb3f23f568f42d6a26761a335a0d56e97e43e8
TOM-BOSS-pinetour <b94889340@gmail.com>
2025-12-15 10:23:19 +0800

    test

--- controllers/category/getCategories.js
+++ controllers/category/getCategories.js
@@ -1,20 +1,16 @@
 const CategoryModel = require("../../schemas/foodCategory");
-const FoodModel = require("../../schemas/food");
 
 const getCategories = async (req, res) => {
   try {
-    const categories = await CategoryModel.find();
-    const foods = await FoodModel.find();
-
-    const data = categories.map((cat) => ({
-      ...cat.toObject(),
-      foods: foods.filter(
-        (food) => food.category?.toString() === cat._id.toString()
-      ),
-    }));
+    const data = await CategoryModel.find().populate("foods");
 
     res.status(200).json(data);
   } catch (err) {
-    res.status(500).json(err.message);
+    res.status(500).json({
+      message: "something went wrong to getCategories",
+      error: err.message,
+    });
   }
 };
+
+module.exports = getCategories;
